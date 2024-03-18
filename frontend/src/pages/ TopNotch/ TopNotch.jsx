5642ac52b6b1e9ef60b8de1ab79e7b18a2ab85da
fasill <fasilhawultie19@gmail.com>
2024-03-18 15:36:26 +0300

    modification: top notch page modifiend

--- frontend/src/pages/ TopNotch/ TopNotch.jsx
+++ frontend/src/pages/ TopNotch/ TopNotch.jsx
@@ -11,34 +11,63 @@ import separator from '../../assets/images/separator.svg';
 const  TopNotch = () => {
   return (
     <div className={style.TopNotchWrapper}> 
-      <div className = {style.cards}>
-        <div className={style.imgwrapper}>
-          <img src = {service1} className={style.service}/>
-        </div>
-        <img src = {serviceBack} className={style.serviceBack}/>
-      </div>
-
-      <div className={style.middle}>
-        <div className={style.midleheader}>
-          <h1>FLAVORS FOR ROYALTY</h1>
-          <img src = {separator} className = {style.separator}/>
-          <h1>We Offer Top Notch</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has been the industrys standard dummy text ever.</p>
-        </div>
+      <div className={style.cardwrapper}>
         <div className = {style.cards}>
           <div className={style.imgwrapper}>
-            <img src = {service3} className={style.service}/>
+            <img src = {service1} className={style.service}/>
           </div>
           <img src = {serviceBack} className={style.serviceBack}/>
         </div>
-      </div>
-      
-      <div className = {style.cards}>
-        <div className={style.imgwrapper}>
-          <img src = {service2} className={style.service}/>
+        <div className={style.bottomtitle}>
+          <h1>Breakfast</h1>
+            <div className={style.menuWrapper}>
+              <p className = {''}>VIEW MENU</p>
+              <div className={style.underline}></div>
+            </div>
         </div>
-        <img src = {serviceBack} className={style.serviceBack}/>
       </div>
+
+      <div className={style.cardwrapper}>
+        <div className={style.middle}>
+          <div className={style.midleheader}>
+            <h1>FLAVORS FOR ROYALTY</h1>
+            <img src = {separator} className = {style.separator}/>
+            <h1>We Offer Top Notch</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry lorem Ipsum has been the industrys standard dummy text ever.</p>
+          </div>
+          <div className = {style.cards}>
+            <div className={style.imgwrapper}>
+              <img src = {service3} className={style.service}/>
+            </div>
+            <img src = {serviceBack} className={style.serviceBack}/>
+          </div>
+        </div>
+        <div className={style.bottomtitle}>
+            <h1>Breakfast</h1>
+              <div className={style.menuWrapper}>
+                <p className = {''}>VIEW MENU</p>
+              <div className={style.underline}></div>
+            </div>
+        </div>
+      </div>
+
+      <div className={style.cardwrapper}>
+          
+        <div className = {style.cards}>
+          <div className={style.imgwrapper}>
+            <img src = {service2} className={style.service}/>
+          </div>
+          <img src = {serviceBack} className={style.serviceBack}/>
+        </div>
+        <div className={style.bottomtitle}>
+              <h1>Breakfast</h1>
+                <div className={style.menuWrapper}>
+                  <p className = {''}>VIEW MENU</p>
+                <div className={style.underline}></div>
+            </div>
+        </div>
+      </div>
+
 
     </div>
   )
